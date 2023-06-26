6bf51f3cc4bbfbfe07e2dec7f9a7e3770c9cd89d
mgonzaleyub <mgonzaleyub@alumnos.unex.es>
2023-06-26 20:50:42 +0200

    style: reestructuration of imgs

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,33 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'mannu3@hotmail.com';
+if(isset($_POST['message'])){
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+    
+	
+	$to      = 'mannu3@hotmail.com';
+	$subject = 'Site Contact Form';
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+	$headers = 'From: '. $email . "\r\n" .
+    'Reply-To: '. $email . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+	$status = mail($to, $subject, $message, $headers);
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+	if($status == TRUE){	
+		$res['sendstatus'] = 'done';
+	
+		//Edit your message here
+		$res['message'] = 'Mail sent successfully';
+    }
+	else{
+		$res['message'] = 'Failed to send mail. Please mail me to mannu3@hotmail.com';
+	}
+	
+	
+	echo json_encode($res);
+}
 
-  echo $contact->send();
 ?>
